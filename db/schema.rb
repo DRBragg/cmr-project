fb1799260d66c8a5e6e618ff9fc90b6a9f4e89f3
Drew Bragg <DRBragg@gmail.com>
2017-09-06 14:15:44 -0400

    fixed schema for codeship error?

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20170905183319) do
-=======
+
 ActiveRecord::Schema.define(version: 20170905185040) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
   create_table "comments", force: :cascade do |t|
     t.text "body", null: false
     t.integer "review_id", null: false
@@ -48,7 +44,7 @@ ActiveRecord::Schema.define(version: 20170905185040) do
     t.integer "representative_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-=======
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -66,7 +62,7 @@ ActiveRecord::Schema.define(version: 20170905185040) do
     t.string "last_name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
->>>>>>> master
+
   end
 
 end
