eeee731b217680a55281ee5dceef02054746105f
Timothy Browne <timbrowne066@gmail.com>
2017-09-13 13:25:06 -0400

    updated lock file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,9 @@ CarrierWave.configure do |config|
   if !Rails.env.test?
     config.fog_credentials = {
       provider: "AWS",
-      aws_access_key_id: "AKIAIE5CEH2UUVDESL3A",
-      aws_secret_access_key: "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"
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
     }
-    config.fog_directory  = "onblastphotos"
+    config.fog_directory  = ENV["S3_BUCKET"]
   end
 end
