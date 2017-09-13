ffcdfeaa49ec0de7411052b9e98b0cd6da5a2a81
Timothy Browne <timbrowne066@gmail.com>
2017-09-13 15:23:21 -0400

    changed domain name

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['app76863324@heroku.com'],
   :password => ENV['vpdwz4bi3234'],
-  :domain => 'onblast.com',
+  :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
