331beadbe168059f12567b4e4879596e2867a605
Drew Bragg <DRBragg@gmail.com>
2017-09-14 15:15:43 -0400

    api tests, api endpoint chagne for reviews

--- spec/controllers/api/v1/reviews_controller_spec.rb
+++ spec/controllers/api/v1/reviews_controller_spec.rb
@@ -1,55 +1,19 @@
-# describe "ReviewsController API" do
-#   it 'displays current reviews' do
-#     FactoryGirl.create_review(:review, 2)
-#
-#     get '/api/v1/reviews'
-#
-#     #test for the 200 status-code
-#     expect(response).to be_success
-#
-#     #check to make sure the right amount of messages are returned
-#     expect(json['reviews'].length).to eq(2)
-#     expect(json['content']).to eq(representative.content)
-#   end
-
-  # it 'displays one representative' do
-  #   representative = FactoryGirl.create(:representative)
-  #   get "/api/v1/representatives/#{representative.id}"
-  #
-  #   #test for the 200 status-code
-  #   expect(response).to be_success
-  #
-  #   #check that the representative attributes are the same
-  #   expect(json['content']).to eq(representative.content)
-  # end
-# end
-
-
 require "rails_helper"
 
 RSpec.describe Api::V1::ReviewsController, type: :controller do
-  # let!(:first_review) = { Review.create(headline: "Hello this is Dog", body: "This is really the very best dog") }
-  # let!(:second_review) = { Review.create(headline: "Pug Swimming", body: "This dog may be even cuter than the other one") }
-
   describe "POST#create" do
+    let!(:user2) {FactoryGirl.create(:user)}
+    let!(:representative2) {FactoryGirl.create(:representative, user: user2)}
     it "creates a new Review" do
-      post_json = { headline: "Beth is coming to town", body: "You better watch out, you better not cry" }.to_json
+      expect{ post :create, params: {representative_id: representative2.id, review: { headline: "You better watch out, you better not cry", body: "this is my reviews", rating: 3, representative_id: representative2.id, user_id: user2.id } }, as: :json }.to change{ Review.count }.by 1
 
-      expect{ post(:create)}.to change{ Review.count }.by 1
+      json_response = JSON.parse(response.body)
+      expect(response.status).to eq 200
+      expect(response.content_type).to eq("application/json")
+
+      expect(json_response).to be_kind_of(Hash)
+      expect(json_response).to_not be_kind_of(Array)
+      expect(json_response["headline"]).to eq "You better watch out, you better not cry"
     end
-  end
-
-  it "returns the json of the newly posted review" do
-    post_json = { headline: "Beth is coming to town", body: "You better watch out, you better not cry" }.to_json
-
-    post(:create)
-    returned_json = JSON.parse(response.body)
-    expect(response.status).to eq 200
-    expect(response.content_type).to eq("application/json")
-
-    expect(returned_json).to be_kind_of(Hash)
-    expect(returned_json).to_not be_kind_of(Array)
-    expect(returned_json["headline"]).to eq "Beth is coming to town"
-    expect(returned_json["body"]).to eq "You better watch out, you better not cry"
   end
 end
