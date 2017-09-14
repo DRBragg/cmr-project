3916954f0e3d7843c1424f637e2a17a9b3a63482
Drew Bragg <DRBragg@gmail.com>
2017-09-14 14:35:11 -0400

    API testing

--- spec/controllers/api/v1/comments_controller_spec.rb
+++ spec/controllers/api/v1/comments_controller_spec.rb
@@ -1,55 +1,22 @@
-# describe "CommentsController API" do
-#   it 'displays current comments' do
-#     FactoryGirl.create_comments(:comment, 2)
-#
-#     get '/api/v1/comments'
-#
-#     #test for the 200 status-code
-#     expect(response).to be_success
-#
-#     #check to make sure the right amount of messages are returned
-#     expect(json['comments'].length).to eq(2)
-#     expect(json['content']).to eq(comment.content)
-#   end
-
-  # it 'displays one representative' do
-  #   representative = FactoryGirl.create(:representative)
-  #   get "/api/v1/comments/#{comment.id}"
-  #
-  #   #test for the 200 status-code
-  #   expect(response).to be_success
-  #
-  #   #check that the representative attributes are the same
-  #   expect(json['content']).to eq(representative.content)
-  # end
-# end
-
-
-
 require "rails_helper"
 
 RSpec.describe Api::V1::CommentsController, type: :controller do
-  # let!(:first_comment) = { Comment.create(body: "This is really the very best dog") }
-  # let!(:second_comment) = { Comment.create(body: "This dog may be even cuter than the other one") }
+  let!(:user1) {FactoryGirl.create(:user)}
+  let!(:representative1) {FactoryGirl.create(:representative, user: user1)}
+  let!(:review1) {FactoryGirl.create(:review, representative: representative1, user: user1)}
 
   describe "POST#create" do
     it "creates a new Comment" do
-      post_json = { body: "You better watch out, you better not cry" }.to_json
 
-      expect{ post (:create)}.to change{ Comment.count }.by 1
+      expect{ post :create, params: { representative_id: representative1.id, review_id: review1.id, comment: { body: "You better watch out, you better not cry", review_id: review1.id, user_id: user1.id } }, as: :json }.to change{ Comment.count }.by 1
+
+      json_response = JSON.parse(response.body)
+      expect(response.status).to eq 200
+      expect(response.content_type).to eq("application/json")
+
+      expect(json_response).to be_kind_of(Hash)
+      expect(json_response).to_not be_kind_of(Array)
+      expect(json_response["body"]).to eq "You better watch out, you better not cry"
     end
-  end
-
-  it "returns the json of the newly posted review" do
-    post_json = { body: "You better watch out, you better not cry" }.to_json
-
-    post(:create)
-    returned_json = JSON.parse(response.body)
-    expect(response.status).to eq 200
-    expect(response.content_type).to eq("application/json")
-
-    expect(returned_json).to be_kind_of(Hash)
-    expect(returned_json).to_not be_kind_of(Array)
-    expect(returned_json["body"]).to eq "You better watch out, you better not cry"
   end
 end
