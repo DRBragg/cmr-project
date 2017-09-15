4e3bbe806dfb272c6b613eb98bf3fa814e779577
Drew Bragg <DRBragg@gmail.com>
2017-09-15 14:24:37 -0400

    merica

--- app/javascript/bundles/IndexPage/components/PageTitle.js
+++ app/javascript/bundles/IndexPage/components/PageTitle.js
@@ -5,7 +5,7 @@ import { Row, Col } from 'react-bootstrap';
 const PageTitle = (props) => {
   return(
     <div>
-        <h1>{props.title}</h1>
+        <h1 className="rep-title">{props.title}</h1>
     </div>
   )
 }
--- app/javascript/bundles/ShowPage/components/PageTitle.js
+++ app/javascript/bundles/ShowPage/components/PageTitle.js
@@ -6,7 +6,7 @@ const PageTitle = (props) => {
   return(
     <Row>
       <Col xs={12} className="text-center">
-        <h1>{props.title}</h1>
+        <h1 className="rep-title">{props.title}</h1>
       </Col>
     </Row>
   )
